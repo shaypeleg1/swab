13c12220fa01baafe7efbdd6b3f2ef324c6fc1e8
Shay Peleg <shaypeleg1@gmail.com>
2017-01-22 23:02:44 +0200

    Added dit to gitignore

---
+++ dist/swab/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+
+var port = process.env.PORT || 5000;
+
+app = express();
+app.use('/', express.static(__dirname));
+
+require('./server-full');
+
+app.use('/*', express.static(__dirname));
+
+app.listen(port, function () {
+  console.log('server started ' + port);
+});
