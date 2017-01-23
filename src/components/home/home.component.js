9727117d2dd3945af9d1866c2fc127cc03f221c0
Shay Peleg <shaypeleg1@gmail.com>
2017-01-23 16:42:33 +0200

    added bulma

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -10,16 +10,17 @@ export default {
   name: 'home-component',
   data: () => {
     return {
-      isGuest : true,
     }
   },
   methods: {
     makeNewSite() {
-      // this dispatch a post request 
       this.$store.dispatch('makeNewSite');
     },
     signIn() {
         this.$router.push({ name: 'signin'});
+    },
+    signUp() {
+        this.$router.push({ name: 'signup'});
     },
     signOut() {
       this.$store.dispatch('signOut')
@@ -32,10 +33,6 @@ export default {
         siteId: this.$store.state.defualtSiteId,
         userId: this.$store.state.currUser.user.user._id
       })
-        // .then((res) => {
-        //   console.log('this is the res:', res);
-        //   this.$router.push('/main/' + this.$store.state.site._id)
-        // });
     }
   },
   computed: {
@@ -53,10 +50,8 @@ export default {
       this.$store.dispatch('checkUserLogged',)
       console.log('isLoggedIn', this.isLoggedIn);      
     if (this.isLoggedIn) {
-      this.isGuest = false;  
       this.$store.dispatch('getSites', this.currUser.sites);
     } else {
-      this.isGuest = true;
     }
   },
 }
